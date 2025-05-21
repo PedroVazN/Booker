b9961d4e983cd05d601368194a09494486f13043
PedroVazN <vaznascimento23@gmail.com>
2025-05-21 15:59:19 -0300

    ..

---
+++ src/pages/CursoInvestimento.tsx
@@ -0,0 +1,3 @@
+  imageUrl: "https://aws-assets.kiwify.com.br/cdn-cgi/image/fit=scale-down,width=1000/fPC2EqvXuzZwSWA/img_builder_44679afb-66ea-44ae-83d9-15afbb957a68_871022d88af74ceeb21f168e427072fe.png",
+  kiwifyLink: "https://pay.kiwify.com.br/cTh1aoy?afid=vHib36ed",
+  videoUrl: "https://youtu.be/HeANxPxj3cE", 
